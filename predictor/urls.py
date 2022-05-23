29e1047fefbcf1603191bc874de1d887942bcb22
Husain-Jinia <husain.jinia0201@gmail.com>
2022-05-23 10:31:02 +0530

    results url

--- predictor/urls.py
+++ predictor/urls.py
@@ -6,6 +6,6 @@ from .views import predictor,result
 urlpatterns = [
   
     path('home/', predictor, name="home"),
-    path('result/<int:pk>',result, name="result")
-
+    path('result/<int:pk>',result, name="result"),
+    path('result_pdf',result_pdf, name="resultpdf")
 ]
